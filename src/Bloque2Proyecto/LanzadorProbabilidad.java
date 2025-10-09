e802e39e93cca48f8508a363f7a5d7326fdd21f1
gn6ks <pabguifon@gmail.com>
2025-10-09 11:30:25 +0200

    bloque 2 proyecto acabado, calculador de probabilidad de colision de NEOs pasado por parametro y multiproceso

---
+++ src/Bloque2Proyecto/LanzadorProbabilidad.java
@@ -0,0 +1,52 @@
+package Bloque2Proyecto;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LanzadorProbabilidad {
+    public static void ejecutar(String linea) {
+        try {
+            String javaHome = System.getProperty("java.home");
+            String javaBin = javaHome + File.separator + "bin" + File.separator + "java";
+            String classpath = System.getProperty("java.class.path");
+            String clase = "Bloque2Proyecto.probabilidadColision";
+            List<String> command = new ArrayList<>();
+            command.add(javaBin);
+            command.add("-cp");
+            command.add(classpath);
+            command.add(clase);
+            command.add(linea);
+            ProcessBuilder builder = new ProcessBuilder(command);
+            Process process = builder.inheritIO().start(); //heredo la terminal de probabilidadColision para mostrar mensajes
+            process.waitFor(); //esto lo he encontrado y es para dejar que se ejecuten todos los procesos
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        //pasar el archivo a leer por parametro, hace que el programa dependa del usuario sin cambiar el codigo
+        String archivoLeer = args[0];
+        long puntoPartidaTiempo = 0;
+        long puntoFinalTiempo = 0;
+        try {
+            puntoPartidaTiempo = System.currentTimeMillis();
+            BufferedReader br = new BufferedReader(new FileReader(archivoLeer));
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                ejecutar(linea); // Lanzar un proceso por linea
+            }
+            puntoFinalTiempo = System.currentTimeMillis();
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Todos los procesos han terminado.");
+        System.out.println("Tiempo de ejecución: " + (puntoFinalTiempo - puntoPartidaTiempo) + " milisegundos");
+        System.out.println("Tiempo medio de ejecución por proceso: " + ((puntoFinalTiempo - puntoPartidaTiempo) / 24)+ " milisegundos");
+    }
+}
---
+++ src/Bloque2Proyecto/probabilidadColision.java
@@ -0,0 +1,44 @@
+package Bloque2Proyecto;
+
+import java.io.*;
+import java.text.DecimalFormat;
+
+public class probabilidadColision {
+    public static int calcularProbabilidad(double posicioNEO, double velocitatNEO) {
+        double posicioTerra = 1;
+        double velocitatTerra = 100;
+        for (int i = 0; i < (50 * 365 * 24 * 60 * 60); i++) {
+            posicioNEO = posicioNEO + velocitatNEO * i;
+            posicioTerra = posicioTerra + velocitatTerra * i;
+        }
+        double resultat = 100 * Math.random() * Math.pow(((posicioNEO - posicioTerra) / (posicioNEO + posicioTerra)), 2);
+        return (int) resultat;
+    }
+
+    public static void main(String[] args) {
+        String linea = args[0];
+        //separar string en array de string donde cada posicion tiene los valores que necesito
+        String[] datos = linea.split(",");
+        String nombreNeo = datos[0];
+        double posicionNeo = Double.parseDouble(datos[1]); //paso a double pq se usan decimales para manejar la calculadora
+        double velocidadNeo = Double.parseDouble(datos[2]); //paso a double pq se usan decimales para manejar la calculadora
+
+        int resultado = calcularProbabilidad(posicionNeo, velocidadNeo); // calcular probabilidad
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(nombreNeo))) {
+            bw.write(String.valueOf(resultado)); //escribir el resultado en el fichero
+            bw.newLine();
+            if (resultado >= 10) {
+                System.out.println("El resultado es mayor a 10% de probabiliad");
+                DecimalFormat df = new DecimalFormat("#.00");
+                System.out.println(nombreNeo + " tiene " + df.format(resultado) + "% de probabilidad de colision");
+            } else {
+                System.out.println("El resultado es menor a 10% de probabilidad");
+                System.out.println(nombreNeo + " se encuentra lejos de colisionar con la tierra :D");
+            }
+        } catch (Exception e) {
+            System.err.println("Error al escribir el fichero para " + nombreNeo);
+            e.printStackTrace();
+        }
+    }
+}
